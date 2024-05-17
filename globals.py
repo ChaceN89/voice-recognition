712f1ff63b7f30930d1930581b47d8bc12735ab7
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-05-17 09:32:50 -0600

    changed the threshold values to make the pass easier

--- globals.py
+++ globals.py
@@ -4,13 +4,13 @@ host = 'http://127.0.0.1:8050/'
 
 audio_model_folder = "audio_models"
 
-CI=0.95
+CI=0.90
 # a 95% confidence level is less likely to accept the test case compared to a 90% confidence level. Here's why:
 # A 95% confidence level corresponds to a larger critical value (approximately 1.96 for a two-tailed test).
 # A 90% confidence level corresponds to a smaller critical value (approximately 1.645 for a two-tailed test).
 
 # values to initially set a threshold value foe max difference in mean and std allowed before rejecting
 mean_threshold = 30  
-std_threshold = 3
+std_threshold = 5
 
 # the CI and z score test is normalized so it will be accepted more often 
